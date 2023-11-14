e9cecc3a88ea3498869698c1decb40e622a4876e
CarlosUribe225 <carurbe225@gmail.com>
2023-11-14 01:24:20 +0100

    added function shootPlayer in gameplay.js

--- src/pages/GameplayPage/gameplay.js
+++ src/pages/GameplayPage/gameplay.js
@@ -1,20 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const astronautList = document.getElementById('astronautList');
-  const players = JSON.parse(localStorage.getItem("players"));
+    const astronautList = document.getElementById('astronautList');
+    const shootButton = document.getElementById('shootButton');
+    const players = JSON.parse(localStorage.getItem("players"));
 
-  if (players && players.length > 0) {
-      const astronautGrid = document.createElement('div');
-      astronautGrid.classList.add('astronaut-grid'); 
+    if (players && players.length > 0) {
+        const astronautGrid = document.createElement('div');
+        astronautGrid.classList.add('astronaut-grid');
 
-      players.forEach((player) => {
-          const astronautDiv = document.createElement('div');
-          astronautDiv.innerHTML = `
-              <img src="../../assets/images/astronaut.png" alt="Astronaut Avatar">
-              <p>${player}</p>
-          `;
-          astronautGrid.appendChild(astronautDiv);
-      });
+        players.forEach((player) => {
+            const astronautDiv = document.createElement('div');
+            astronautDiv.classList.add('astronaut-item');
+            astronautDiv.innerHTML = `
+                <img src="../../assets/images/astronaut.png" alt="Astronaut Avatar" data-player="${player}">
+                <p>${player}</p>`;
+            astronautGrid.appendChild(astronautDiv);
+        });
 
-      astronautList.appendChild(astronautGrid);
-  }
+        astronautList.appendChild(astronautGrid);
+        shootButton.addEventListener('click', shootPlayer);
+    }
 });
+
+function shootPlayer() {
+    const astronautItems = document.querySelectorAll('.astronaut-item');
+
+    if (astronautItems.length > 0) {
+        const randomIndex = Math.floor(Math.random() * astronautItems.length);
+        const eliminatedPlayer = astronautItems[randomIndex].querySelector('img').getAttribute('data-player');
+
+        astronautItems[randomIndex].querySelector('img').src = '../../assets/images/eliminate-player.png';
+
+        alert(`El jugador ${eliminatedPlayer} ha sido eliminado.`);
+    } else {
+        alert('No hay jugadores para eliminar.');
+    }
+}
