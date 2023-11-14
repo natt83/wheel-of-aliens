e1c8d14805237a22a1952afbe24668b1e00ff3a7
CarlosUribe225 <carurbe225@gmail.com>
2023-11-14 03:22:15 +0100

    added results board in results.js

--- src/pages/GameplayPage/gameplay.js
+++ src/pages/GameplayPage/gameplay.js
@@ -1,4 +1,12 @@
+function clearOldData() {
+    localStorage.removeItem('roundEliminated');
+    localStorage.removeItem('results');
+}
+
+    
+
 document.addEventListener('DOMContentLoaded', function() {
+    clearOldData();
     const astronautList = document.getElementById('astronautList');
     const shootButton = document.getElementById('shootButton');
     let players = JSON.parse(localStorage.getItem("players")) || [];
@@ -21,20 +29,40 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+
 function shootPlayer() {
     const astronautItems = document.querySelectorAll('.astronaut-item');
 
     if (astronautItems.length > 0) {
         const availablePlayers = Array.from(astronautItems).filter(item => !item.classList.contains('eliminated'));
-        
-        if (availablePlayers.length > 0) {
+
+        if (availablePlayers.length > 1) {
             const randomIndex = Math.floor(Math.random() * availablePlayers.length);
-            const eliminatedPlayer = availablePlayers[randomIndex].querySelector('img').getAttribute('data-player');
+            const eliminatedPlayerElement = availablePlayers[randomIndex];
+            const eliminatedPlayer = eliminatedPlayerElement.querySelector('img').getAttribute('data-player');
+            const roundEliminated = localStorage.getItem('roundEliminated') || 0;
 
-            availablePlayers[randomIndex].classList.add('eliminated');
-            availablePlayers[randomIndex].querySelector('img').src = '../../assets/images/eliminate-player.png';
+            eliminatedPlayerElement.classList.add('eliminated');
+            eliminatedPlayerElement.querySelector('img').src = '../../assets/images/eliminate-player.png';
 
-            alert(`El jugador ${eliminatedPlayer} ha sido eliminado.`);
+            if (availablePlayers.length === 2) {
+                const remainingPlayerElement = availablePlayers.find(item => !item.classList.contains('eliminated')).querySelector('img');
+                const remainingPlayer = remainingPlayerElement.getAttribute('data-player');
+                remainingPlayerElement.src = '../../assets/images/winner.jpg';
+
+                alert(`¡El jugador ${remainingPlayer} ha derrotado a los aliens!`);
+
+                // Almacena el resultado del ganador
+                storeResult(remainingPlayer, roundEliminated + 1);
+            } else {
+                alert(`El jugador ${eliminatedPlayer} ha sido eliminado.`);
+
+                // Almacena el resultado del jugador eliminado
+                storeResult(eliminatedPlayer, roundEliminated + 1);
+
+                // Incrementa el número de la ronda
+                localStorage.setItem('roundEliminated', roundEliminated + 1);
+            }
         } else {
             alert('Todos los jugadores han sido eliminados.');
         }
@@ -42,3 +70,15 @@ function shootPlayer() {
         alert('No hay jugadores para eliminar.');
     }
 }
+
+function storeResult(player, round) {
+    // Obtiene los resultados almacenados actualmente
+    const results = JSON.parse(localStorage.getItem('results')) || [];
+
+    // Almacena el resultado del jugador
+    results.push({ player, round });
+
+    // Actualiza los resultados en localStorage
+    localStorage.setItem('results', JSON.stringify(results));
+}
+
